d65cef53bbfc8a81cd9fa03cada3034959a27bb7
xiaofeicao <xiaofeicao@microsoft.com>
2023-07-07 00:36:45 +0800

    init

---
+++ src/ConversationHandler.ts
@@ -0,0 +1,66 @@
+import { Activity, TurnContext, TeamsInfo } from "botbuilder";
+import { CommandMessage, TeamsFxBotCommandHandler, TriggerPatterns } from "@microsoft/teamsfx";
+const { OpenAI } = require("langchain/llms/openai");
+const { OpenAIEmbeddings } = require("langchain/embeddings/openai");
+const { RecursiveCharacterTextSplitter } = require("langchain/text_splitter");
+const { BufferMemory } = require("langchain/memory");
+
+import {
+  ActivityTypes,
+} from 'botframework-schema';
+const { HNSWLib } = require("langchain/vectorstores/hnswlib");
+import * as fs from "fs";
+const { ConversationalRetrievalQAChain } = require("langchain/chains");
+
+export class ConversationHandler implements TeamsFxBotCommandHandler {
+  triggerPatterns: TriggerPatterns = ".*";
+  chat = new OpenAI({ maxTokens: 256 })
+  chain;
+
+  async handleCommandReceived(
+    context: TurnContext,
+    message: CommandMessage
+  ): Promise<string | Partial<Activity> | void> {
+
+    console.log(`App received message: ${message.text}`);
+
+    const msg: Partial<Activity> = {
+      type: ActivityTypes.Message,
+    };
+
+    if(!this.chain) {
+
+      const embeddings = new OpenAIEmbeddings({
+        azureOpenAIApiDeploymentName: process.env.AZURE_OPENAI_EMBEDDING_DEPLOYMENT_NAME,
+        modelName: process.env.AZURE_OPENAI_EMBEDDING_MODEL_NAME,
+      });
+      const content = fs.readFileSync("docs/docs.txt", "utf8");
+
+      const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000 });
+      const docs = await textSplitter.createDocuments([content]);
+      /* Create the vectorstore */
+      const vectorStore = await HNSWLib.fromDocuments(docs, embeddings);
+      this.chain = ConversationalRetrievalQAChain.fromLLM(
+        this.chat,
+        vectorStore.asRetriever(),
+        {
+          returnSourceDocuments: false,
+          memory: new BufferMemory({
+            memoryKey: "chat_history", // Must be set to "chat_history"
+            inputKey: "question", // The key for the input to the chain
+            outputKey: "text", // The key for the final conversational output of the chain
+          }),
+          verbose: true
+        },
+      );
+    }
+
+    const caller = await TeamsInfo.getMember(context, context.activity.from.id);
+
+    const text = await this.chain.call({question: message.text})
+    console.log(text)
+
+    msg.text = "@" + caller.name + " " + text.text
+    return msg
+  }
+}
---
+++ src/internal/config.ts
@@ -0,0 +1,8 @@
+const config = {
+  botId: process.env.BOT_ID,
+  botPassword: process.env.BOT_PASSWORD,
+  azureOpenAIApiDeploymentName: process.env.AZURE_OPENAI_EMBEDDING_DEPLOYMENT_NAME,
+  embeddingModelName: process.env.AZURE_OPENAI_EMBEDDING_MODEL_NAME
+};
+
+export default config;
